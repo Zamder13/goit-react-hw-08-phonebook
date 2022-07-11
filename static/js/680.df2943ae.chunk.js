e0420403ed4fe14e681c7de26957beb744e15468
Zamder13 <Zamder@ukr.net>
2022-07-11 17:15:04 +0300

    Updates

---
+++ static/js/680.df2943ae.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,n,r){r.r(n);var c=r(5048),t=r(184);n.default=function(){var e=(0,c.v9)((function(e){return e.users.user.name}));return(0,t.jsx)(t.Fragment,{children:e?(0,t.jsxs)("h1",{children:["Welcome ",e]}):(0,t.jsx)("h1",{children:"Welcome citizen"})})}}}]);
+//# sourceMappingURL=680.df2943ae.chunk.js.map
